2efb05b4a36e6c9e7344ee7a8ce0bd5a21570363
Daniel Araujo Chaves <danielaraujo.da789@gmail.com>
2026-07-30 17:47:46 -0300

    :rocket: Deploy do Projeto Farmacia

--- src/main/java/com/generation/farmacia/security/SecurityConfig.java
+++ src/main/java/com/generation/farmacia/security/SecurityConfig.java
@@ -19,48 +19,50 @@ import jakarta.servlet.http.HttpServletResponse;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-	
-	private static final String[] PUBLIC_ENDPOINTS = {
-	        "/usuarios/logar",
-	        "/usuarios/cadastrar",
-	        "/error/**",
-	        "/", "/docs", "/swagger-ui/**", "/v3/api-docs/**", "/swagger-resources/**"
-	    };
 
-	    @Autowired
-	    private JwtAuthFilter jwtAuthFilter;
+    private static final String[] PUBLIC_ENDPOINTS = {
+    		"/usuarios/logar",
+    	    "/usuarios/cadastrar",
+    	    "/error/**",
+    	    "/",
+    	    "/docs",
+    	    "/swagger-ui/**",
+    	    "/swagger-ui.html",
+    	    "/v3/api-docs",
+    	    "/v3/api-docs/**",
+    	    "/swagger-resources/**"
+    };
 
-	    @Bean
-	    PasswordEncoder passwordEncoder() {
-	        return new BCryptPasswordEncoder(10);
-	    }
+    @Autowired
+    private JwtAuthFilter jwtAuthFilter;
 
-	    @Bean
-	    AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
-	        return config.getAuthenticationManager();
-	    }
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(10);
+    }
 
-	    @Bean
-	    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-	        return http
-	            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-	            .csrf(csrf -> csrf.disable())
-	            .cors(cors -> {}) // Aqui é para liberar o acesso do front-end
-	            
-	            .authorizeHttpRequests(auth -> auth
-	                .requestMatchers(PUBLIC_ENDPOINTS).permitAll()
-	                .requestMatchers(HttpMethod.OPTIONS).permitAll()
-	                .anyRequest().authenticated()
-	            )
-	            
-	            .exceptionHandling(exceptions -> exceptions
-	                    .authenticationEntryPoint((request, response, authException) -> 
-	                        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, 
-	                            "Não autorizado - Token JWT ausente ou inválido"))
-	            )
-	            
-	            .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
-	            .build();
-	    }
-	    
-	}
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .csrf(csrf -> csrf.disable())
+            .cors(cors -> {})
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers(PUBLIC_ENDPOINTS).permitAll()
+                .requestMatchers(HttpMethod.OPTIONS).permitAll()
+                .anyRequest().authenticated()
+            )
+            .exceptionHandling(exceptions -> exceptions
+                .authenticationEntryPoint((request, response, authException) -> 
+                    response.sendError(HttpServletResponse.SC_UNAUTHORIZED, 
+                        "Não autorizado - Token JWT ausente ou inválido"))
+            )
+            .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+            .build();
+    }
+}
